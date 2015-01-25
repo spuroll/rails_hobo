e36c415f1df97dbdf7046345b272f14068a145bc
Steven <stevenpuroll@gmail.com>
2015-01-25 04:54:58 +0000

    Modify 'new' view. Add 'new' and 'create' actions to ArticlesController.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,16 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to(:action => 'index')
+    else
+      flash[:notice] = "Please ensure you've completed both fields."
+      render('new')
+    end    
   end
 
   def show
@@ -8,4 +19,10 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+  
 end
